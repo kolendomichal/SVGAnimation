5888b3d5015bd84442290ff3152f2b5ef647e54e
Michał Kolendo <01125147@pw.edu.pl>
2019-05-03 18:44:03 +0200

    Actor list, selected Actor show edition panel with xPosition change, inputRange

--- animation-svg/src/animation/SVGAnimation.js
+++ animation-svg/src/animation/SVGAnimation.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./SVGAnimation.css";
-import SVGActors from "./actors/SVGActors";
+import SVGActors from "./SVGActors/SVGActors";
 import SVGActorEditor from "./SVGActorEditor/SVGActorEditor";
+import { Actor } from "./static/actor";
 
 class SVGAnimation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedActor: "actor",
+      selectedActor: null,
       actors: []
     };
   }
@@ -15,18 +16,35 @@ class SVGAnimation extends React.Component {
     console.log("SVGAnimation Mounted");
   }
 
-  addActor = (actor) => {
+  addActor = () => {
+    let actor = new Actor();
+    actor.id = this.state.actors.length + 1;
     this.setState(prevState => ({
       actors: [...prevState.actors, actor]
     }));
   }
+  showActorEditor = (id) => {
+    let selectedActor = this.state.actors.find(actor => actor.id === id);
+    this.setState({selectedActor});
+  }
 
-  renderActors(){
-    return <div>
-              {this.state.actors.map(function (item, i){
-                return <div>{item}</div>
-              })}
-      </div>
+  renderActorsList = () =>{
+    return this.state.actors.map( (item, key) => {
+      return (
+        <li key={item.id} className="list-group-item list-actor" onClick={() => this.showActorEditor(item.id)}>{item.name} {item.id}</li>
+      );
+    });
+  }
+
+  changeXposition = (xPosition) => {
+    let actors = [...this.state.actors];
+    let selectedActor = this.state.selectedActor;
+    let selectedActorIndex = actors.findIndex(actor => actor.id === selectedActor.id);
+
+    selectedActor.xPosition = xPosition;
+    actors[selectedActorIndex] = selectedActor;
+
+    this.setState({actors,selectedActor});
   }
   render() {
     console.log("SVGAnimation rendered");
@@ -34,11 +52,14 @@ class SVGAnimation extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-3 p-0">
-            <SVGActors addActor={this.addActor} />
+            <SVGActors 
+              addActor={this.addActor} 
+              renderActorsList={this.renderActorsList} />
           </div>
-          <div className="col-lg-4 ">
-          {this.renderActors()}
-            {/* <SVGActorEditor addActor={this.addActor} /> */}
+          <div className="col-lg-4 bg-light">
+           <SVGActorEditor 
+              changeXposition={this.changeXposition}
+              selectedActor={this.state.selectedActor} /> 
           </div>
           <div className="col-lg-5 " />
         </div>
