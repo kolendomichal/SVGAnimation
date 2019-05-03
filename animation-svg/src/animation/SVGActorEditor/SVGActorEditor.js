f75ec2449d96b1a506646f03282a22c39a797c8d
Michał Kolendo <01125147@pw.edu.pl>
2019-05-03 18:59:31 +0200

    inputslider start from correct position

--- animation-svg/src/animation/SVGActorEditor/SVGActorEditor.js
+++ animation-svg/src/animation/SVGActorEditor/SVGActorEditor.js
@@ -12,28 +12,45 @@ class SVGActorEditor extends React.Component {
     };
   }
 
+  //TODO Deprecated method, use getDerivedPropsFromState
+  //This function is for slider to start from proper position
+  componentWillReceiveProps(newProps) {
+    if (this.props.selectedActor !== null && newProps.selectedActor !== null) {
+      if (newProps.selectedActor.id !== this.props.selectedActor.id) {
+        this.setState({ xPosition: newProps.selectedActor.xPosition });
+      }
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         {this.props.selectedActor && (
-        <React.Fragment>
-          <div className="ml-2 mt-4 text-dark"> X position </div>
-          <div className="row">
-            <div className="col-lg-9 mt-4 ">
-              <InputRange
-                minValue={0}
-                maxValue={250}
-                value={this.state.xPosition}
-                onChange={(xPosition) => this.setState({xPosition})}
-                onChangeComplete={(xPosition) => this.props.changeXposition(xPosition)}
-              />
+          <React.Fragment>
+            <div className="ml-2 mt-4 text-dark"> X position </div>
+            <div className="row">
+              <div className="col-lg-9 mt-4 ">
+                <InputRange
+                  minValue={0}
+                  maxValue={250}
+                  value={this.state.xPosition}
+                  onChange={xPosition => this.setState({ xPosition })}
+                  onChangeComplete={xPosition =>
+                    this.props.changeXposition(xPosition)
+                  }
+                />
+              </div>
+              <div className="col-lg-3 mt-3">
+                {/* TODO handle onChange in input  */}
+                <input
+                  type="text"
+                  className="form-control"
+                  value={this.props.selectedActor.xPosition}
+                  onChange={value => this.props.changeXposition(value)}
+                />
+              </div>
             </div>
-            <div className="col-lg-3 mt-3">
-               {/* TODO handle onChange in input  */}
-              <input type="text" className="form-control" value={this.props.selectedActor.xPosition} onChange={(value) => this.props.changeXposition(value)} />
-            </div>
-          </div>
-        </React.Fragment>
+          </React.Fragment>
         )}
       </React.Fragment>
     );
