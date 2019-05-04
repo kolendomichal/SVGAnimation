1e46ebc4fad0b81a2350266b21512febf47c14cb
Michał Kolendo <mchkolendo@gmail.com>
2019-05-04 09:55:11 +0200

    refactored SVGActorEditor to render inputNumberRange components, todo change InputNumberRange Component to use react Hook

--- animation-svg/src/animation/SVGActorEditor/SVGActorEditor.js
+++ animation-svg/src/animation/SVGActorEditor/SVGActorEditor.js
@@ -1,60 +1,28 @@
 import React from "react";
 import "./SVGActorEditor.css";
-import "./input-range.css";
+import InputNumberRange from "./InputNumberRange";
 
-import InputRange from "react-input-range";
-
-class SVGActorEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      xPosition: 0
-    };
-  }
-
-  //TODO Deprecated method, use getDerivedPropsFromState
-  //This function is for slider to start from proper position
-  componentWillReceiveProps(newProps) {
-    if (this.props.selectedActor !== null && newProps.selectedActor !== null) {
-      if (newProps.selectedActor.id !== this.props.selectedActor.id) {
-        this.setState({ xPosition: newProps.selectedActor.xPosition });
-      }
-    }
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        {this.props.selectedActor && (
-          <React.Fragment>
-            <div className="ml-2 mt-4 text-dark"> X position </div>
-            <div className="row">
-              <div className="col-lg-9 mt-4 ">
-                <InputRange
-                  minValue={0}
-                  maxValue={250}
-                  value={this.state.xPosition}
-                  onChange={xPosition => this.setState({ xPosition })}
-                  onChangeComplete={xPosition =>
-                    this.props.changeXposition(xPosition)
-                  }
-                />
-              </div>
-              <div className="col-lg-3 mt-3">
-                {/* TODO handle onChange in input  */}
-                <input
-                  type="text"
-                  className="form-control"
-                  value={this.props.selectedActor.xPosition}
-                  onChange={value => this.props.changeXposition(value)}
-                />
-              </div>
-            </div>
-          </React.Fragment>
-        )}
-      </React.Fragment>
-    );
-  }
+function SVGActorEditor(props) {
+  return (
+    <React.Fragment>
+      {props.selectedActor && (
+        <React.Fragment>
+          <InputNumberRange
+            type="X Position"
+            changeSpecifiedValue={props.changeXposition}
+            selectedActor={props.selectedActor}
+            value={props.selectedActor.xPosition}
+          />
+          <InputNumberRange
+            type="Y Position"
+            changeSpecifiedValue={props.changeYposition}
+            selectedActor={props.selectedActor}
+            value={props.selectedActor.yPosition}
+          />
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default SVGActorEditor;
