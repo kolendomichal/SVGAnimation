30fe5b5ef6f66dddb9c890847155fccd55ca2943
Michal Kolendo <mchkolendo@gmail.com>
2019-05-11 16:06:48 +0200

    Figure opacity added

--- animation-svg/src/animation/SVGCanvas/SVGCircle.js
+++ animation-svg/src/animation/SVGCanvas/SVGCircle.js
@@ -6,7 +6,7 @@ function SVGCircle(props) {
     const figure = props.figure;
     let animStyleName = Object.keys(AnimationTypes).find(key => AnimationTypes[key] === figure.animationType);
     return (
-        <circle fill={figure.fill.hex} stroke={figure.stroke.hex} strokeWidth={figure.strokeWidth} className={animStyleName + " figure"} cx={figure.xPosition} cy={figure.yPosition} r="50" />
+        <circle opacity={figure.opacity} fill={figure.fill.hex} stroke={figure.stroke.hex} strokeWidth={figure.strokeWidth} className={animStyleName + " figure"} cx={figure.xPosition} cy={figure.yPosition} r="50" />
     )
 }
 
