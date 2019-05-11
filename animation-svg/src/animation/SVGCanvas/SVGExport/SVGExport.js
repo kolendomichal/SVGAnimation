00176f08b647c1395fdeae5ec63b9e7e635a6aa7
Michal Kolendo <mchkolendo@gmail.com>
2019-05-11 22:21:21 +0200

    svg export bootstrap modal

--- animation-svg/src/animation/SVGCanvas/SVGExport/SVGExport.js
+++ animation-svg/src/animation/SVGCanvas/SVGExport/SVGExport.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import Popup from 'reactjs-popup';
 import SVGStyles from './SVGStyles';
 import './SVGExport.css';
 
 function SVGExport(props) {
 
     function addStylesToSVG(svg) {
-        svg = svg.substring(0, svg.length-6);
+        svg = svg.substring(0, svg.length - 6);
         svg += "<style> " + SVGStyles + "</style></svg>";
         return svg;
     }
-    
+
     function copySVGAnimation() {
-        if(props.svgElement === undefined) return '';
+        if (props.svgElement === undefined) return '';
         let svg = ReactDOMServer.renderToStaticMarkup(props.svgElement);
         return addStylesToSVG(svg);
     }
@@ -23,17 +22,33 @@ function SVGExport(props) {
     }
 
     return (
-        <div className="row mt-5 w-100">
-            <div className="col-lg-12 text-center">
-                <span className="h2">SVGAnimation</span>
-                <Popup className="border border-dark"  trigger={<button className="btn btn-secondary float-right mr-2"> Export SVG </button>} modal>
-                    <div className="svg-code w-100">
-                        <p className="h5 font-weight-bold ml-3 mt-3">Copy SVG</p>
-                        <textarea readOnly ref={props.svgText} className="svg-text rounded bg-light w-100 mt-2" onClick={() => handleTextSelect()}>{copySVGAnimation()}</textarea>
-                    </div>
-                </Popup>
+        <React.Fragment>
+            <div className="row mt-5 w-100">
+                <div className="col-lg-12 text-center">
+                    <span className="h2">SVGAnimation</span>
+                    <button type="button" className="btn btn-secondary float-right mr-2" data-toggle="modal" data-target="#exampleModal"> Export SVG </button>
+                </div>
             </div>
-        </div >
+            <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">SVGAnimation</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <textarea value={copySVGAnimation()} readOnly ref={props.svgText} className="svg-text rounded bg-light w-100 mt-2" onClick={() => handleTextSelect()} />
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" data-dismiss="modal">Done</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+
     )
 
 }
