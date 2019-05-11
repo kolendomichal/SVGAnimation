a2f3b2c2d18867908231fa96b2359e38dbc98a29
Michal Kolendo <mchkolendo@gmail.com>
2019-05-11 16:06:48 +0200

    Figure opacity added

--- animation-svg/src/animation/SVGCanvas/SVGSquare.js
+++ animation-svg/src/animation/SVGCanvas/SVGSquare.js
@@ -6,7 +6,7 @@ function SVGSquare(props) {
     const figure = props.figure;
     let animStyleName = Object.keys(AnimationTypes).find(key => AnimationTypes[key] === figure.animationType);
     return (
-        <polygon fill={figure.fill.hex} stroke={figure.stroke.hex} strokeWidth={figure.strokeWidth} points={computeCornerPoints(figure.xPosition, figure.yPosition, 50)} className={animStyleName + " figure"} />
+        <polygon  opacity={figure.opacity}  fill={figure.fill.hex} stroke={figure.stroke.hex} strokeWidth={figure.strokeWidth} points={computeCornerPoints(figure.xPosition, figure.yPosition, 50)} className={animStyleName + " figure"} />
     )
 }
 
