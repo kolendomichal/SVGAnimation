2938a16e3c3d6ecf499f452c9e06a0a3a013f3da
Michal Kolendo <mchkolendo@gmail.com>
2019-05-11 16:19:49 +0200

    Export SVG button { todo functionality }

--- animation-svg/src/animation/SVGCanvas/SVGCanvas.js
+++ animation-svg/src/animation/SVGCanvas/SVGCanvas.js
@@ -26,9 +26,19 @@ function SVGCanvas(props) {
     }
 
     return (
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 800" width="100%" height="100%">
-            {getFiguresToRender()}
-        </svg>
+        <React.Fragment>
+            <div className="row mt-5">
+                <div className="col-lg-8 text-center">
+                    <span className="h2 ml-2">SVGAnimation</span>
+                </div>
+                <div className="col-lg-4">
+                    <button className="btn btn-primary float-right mr-5"> Export SVG </button>
+                </div>
+            </div>
+            <svg version="1.1" className="border mt-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" width="100%" height="80%">
+                {getFiguresToRender()}
+            </svg>
+        </React.Fragment>
     )
 }
 
