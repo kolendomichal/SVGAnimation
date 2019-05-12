9b6c8baa6daef3d70c2c050777cedb027a03955c
krolakb <krolakbartlomiej@gmail.com>
2019-05-12 19:42:39 +0200

    Fixed polygon and square animation in ugly way.

--- animation-svg/src/animation/SVGCanvas/SVGFigures/SVGPolygon.js
+++ animation-svg/src/animation/SVGCanvas/SVGFigures/SVGPolygon.js
@@ -3,23 +3,31 @@ import SVGAnimate from '../SVGAnimate/SVGAnimate'
 
 function SVGPolygon(props) {
     const figure = props.figure;
-    figure.animation.attributeName = "points";
-    figure.animation.from = computeCornerPoints(figure.xPosition, figure.yPosition, figure.size, figure.numOfSides);
-    figure.animation.to = computeCornerPoints(figure.xPosition + 50, figure.yPosition, figure.size, figure.numOfSides);
-
+    
     return (
         <React.Fragment>
             <polygon id={figure.hrefid} opacity={figure.opacity} fill={figure.fill.hex} stroke={figure.stroke.hex} strokeWidth={figure.strokeWidth} points={computeCornerPoints(figure.xPosition, figure.yPosition, figure.size, figure.numOfSides)} className="figure" />
-            <SVGAnimate animation={figure.animation}/>
+            <SVGAnimate animation={figure.animation} 
+                        attributeName="points"
+                        from={computeCornerPoints(figure.xPosition, figure.yPosition, figure.size, figure.numOfSides)}
+                        to={computeToPoints(figure)}
+            />
         </React.Fragment>
     )
+}
+
+function computeToPoints(figure) {
+    if(figure.animation.attributeName === 'cx')
+        return computeCornerPoints(figure.xPosition + parseInt(figure.animation.to), figure.yPosition, figure.size, figure.numOfSides);
+    if(figure.animation.attributeName === 'cy')
+        return computeCornerPoints(figure.xPosition, figure.yPosition + parseInt(figure.animation.to), figure.size, figure.numOfSides);
 }
 
 function computeCornerPoints(xMiddle, yMiddle, radius, numOfSides) {
     let polygonPoints = [];
     for(let i = 0; i < numOfSides; i++) {
-        polygonPoints.push(xMiddle + radius * Math.cos(2 * Math.PI * i / numOfSides),
-                           yMiddle + radius * Math.sin(2 * Math.PI * i / numOfSides));
+        polygonPoints.push(parseInt(xMiddle + radius * Math.cos(2 * Math.PI * i / numOfSides)),
+                           parseInt(yMiddle + radius * Math.sin(2 * Math.PI * i / numOfSides)));
     }
     return polygonPoints;
 }
