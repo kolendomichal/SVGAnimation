baa472c200357ae5f21adcb624f719f7a35e0e40
krolakb <krolakbartlomiej@gmail.com>
2019-05-13 00:40:44 +0200

    click on figure triggers change of selected figure

--- animation-svg/src/animation/SVGCanvas/SVGFigures/SVGCircle.js
+++ animation-svg/src/animation/SVGCanvas/SVGFigures/SVGCircle.js
@@ -5,10 +5,9 @@ function SVGCircle(props) {
     const figure = props.figure;
     return (
         <React.Fragment>
-            <circle id={figure.hrefid} opacity={figure.opacity} fill={figure.fill.hex} stroke={figure.stroke.hex} strokeWidth={figure.strokeWidth} cx={figure.xPosition} cy={figure.yPosition} r={figure.size} />
+            <circle style={{ cursor: 'pointer' }} id={figure.hrefid} opacity={figure.opacity} fill={figure.fill.hex} stroke={figure.stroke.hex} strokeWidth={figure.strokeWidth} cx={figure.xPosition} cy={figure.yPosition} r={figure.size} />
             <SVGAnimate animation={figure.animation}/>
         </React.Fragment>
     )
 }
-
 export default SVGCircle;
