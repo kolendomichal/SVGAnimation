6ef8187ace3623efd436b92aaae379e279f804dd
Michal Kolendo <mchkolendo@gmail.com>
2019-05-13 22:17:10 +0200

    fix commit before

--- animation-svg/src/animation/SVGProjectsFiguresNav/SVGProjectsFiguresNav.js
+++ animation-svg/src/animation/SVGProjectsFiguresNav/SVGProjectsFiguresNav.js
@@ -6,10 +6,10 @@ function SVGProjectsFiguresNav(props) {
     return (
         <ul className="svg-editor-nav nav nav-tabs nav-fill ">
             <li className="nav-item bg-light" onClick={() => props.handleProjectFigureTabChange(true)}>
-                <button className={"project-figure-nav-item btn btn-outline-secondary  h-100 w-100 " + props.isActiveEditor(true)}>Projects</button>
+                <button className={"project-figure-nav-item btn btn-outline-secondary  h-100 w-100 " + props.isActiveProjectFigureTab(true)}>Projects</button>
             </li>
             <li className=" nav-item" onClick={() => props.handleProjectFigureTabChange(false)}>
-                <button className={"project-figure-nav-item btn btn-outline-secondary .btn-outline-* w-100  " + props.isActiveEditor(false)}>Figures</button>
+                <button className={"project-figure-nav-item btn btn-outline-secondary .btn-outline-* w-100  " + props.isActiveProjectFigureTab(false)}>Figures</button>
             </li>
         </ul>
     );
