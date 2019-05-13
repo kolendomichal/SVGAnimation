601e9f066c92d135e402debc98b4f8873238e347
Michal Kolendo <mchkolendo@gmail.com>
2019-05-13 22:26:46 +0200

    SVG Import working for project

--- animation-svg/src/animation/static/Project.js
+++ animation-svg/src/animation/static/Project.js
@@ -3,14 +3,7 @@ const uuidv1 = require('uuid/v1');
 export class Project {
     constructor() {
         this.id = uuidv1();
-        this.name = ("Project " + this.id).substr(0,25);
+        this.name = ("Project " + this.id).substr(0, 25);
         this.figuresList = [];
     }
-    // get id() {
-        // return this.id;
-    // }
-    // set id(value) {
-        // throw new Error("Uuid is immutable");
-    // }
-
 }
