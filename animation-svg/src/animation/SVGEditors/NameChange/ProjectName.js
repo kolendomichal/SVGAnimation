5e3d0319e6134f75965781f4235fcdd3e05dfa0f
Michal Kolendo <mchkolendo@gmail.com>
2019-06-12 11:58:55 +0200

    code refactor ,pdfs generated

--- animation-svg/src/animation/SVGEditors/NameChange/ProjectName.js
+++ animation-svg/src/animation/SVGEditors/NameChange/ProjectName.js
@@ -2,7 +2,7 @@ import React, { useRef ,useContext} from "react";
 import "./ActorName.css"
 import SVGContext from "../../SVGContext";
 
-function ProjectName(props) {
+function ProjectName() {
     const svgContext = useContext(SVGContext);
     const editName = useRef(null);
 
@@ -13,7 +13,7 @@ function ProjectName(props) {
     return (
         <div className="text-center mt-3">
             <button ref={editName} className="h1 actor-project-name" contentEditable="true" onBlur={() => onBlurHandle()} suppressContentEditableWarning={true}>
-                {svgContext.selectedProject[props.valueType]}
+                {svgContext.selectedProject["name"]}
             </button>
         </div>
     );
--- animation-svg/src/animation/SVGEditors/SVGFigureEditor/SVGFigureEditor.js
+++ animation-svg/src/animation/SVGEditors/SVGFigureEditor/SVGFigureEditor.js
@@ -11,9 +11,7 @@ function SVGFigureEditor(props) {
 
   return (
     <div className="figure-editor">
-      <ProjectName
-        valueType="name"
-      />
+      <ProjectName/>
       <ActorName
         valueType="name"
       />
