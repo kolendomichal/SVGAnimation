6e8f1e22438033f425eab377736b0d76768b508b
Z6MVM <MVM@KMDPoland.dk>
2019-07-04 14:20:17 +0200

    Updates

---
+++ precache-manifest.26e87c2400a4894db0dd6be5355615d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3380c16a914cdb42e06e97a80598cc",
+    "url": "/SVGAnimation/index.html"
+  },
+  {
+    "revision": "31513bc565b138dd1219",
+    "url": "/SVGAnimation/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "740818873d84f5bdc98c",
+    "url": "/SVGAnimation/static/css/main.c362d9ed.chunk.css"
+  },
+  {
+    "revision": "31513bc565b138dd1219",
+    "url": "/SVGAnimation/static/js/2.ce66cf00.chunk.js"
+  },
+  {
+    "revision": "740818873d84f5bdc98c",
+    "url": "/SVGAnimation/static/js/main.39ac7d3f.chunk.js"
+  },
+  {
+    "revision": "129add4ad01e9422d5b2",
+    "url": "/SVGAnimation/static/js/runtime~main.2b339243.js"
+  }
+]);
